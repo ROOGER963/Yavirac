5648fe9f703e628f60b0f2db890cd55acb9a2327
butterspc <rfp.pineda@yavirac.edu.ec>
2023-06-19 22:01:38 -0500

    rutas ultimos cambios

--- tercero/pv/dashboard/src/app/pages/pages-routing.module.ts
+++ tercero/pv/dashboard/src/app/pages/pages-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { StudentsComponent } from './students/students.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path:'students', component: StudentsComponent},
+  { path:'students',component:StudentsComponent},
  // { path: 'teachers', component: TeachersComponent },
-  { path:'admin', component: AdminComponent}
+  { path:'admin', component: AdminComponent},
+  { path:'home',component:HomeComponent}
 ];
 
 @NgModule({
