d8b45417dec1f7934d0b47110659fc5e82c263a2
butterspc <rfp.pineda@yavirac.edu.ec>
2024-01-11 18:49:35 -0500

    cerrando

--- tercero/pv/dashboard/src/app/app-routing.module.ts
+++ tercero/pv/dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
-import { AuthRoutingModule } from './auth/auth-routing.module';
+import { AuthRoutingModule } from './pages/auth/auth-routing.module';
 import { PagesRoutingModule } from './pages/pages-routing.module';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './auth/login/login.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
--- tercero/pv/dashboard/src/app/app.module.ts
+++ tercero/pv/dashboard/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule} from '@angular/material/icon';
 import { MatListModule} from '@angular/material/list';
 import { FormsModule } from '@angular/forms';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './pages/auth/auth.module';
 import { PagesModule } from './pages/pages.module';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
